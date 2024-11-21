3ae40de0a0d193451e3a110306e4a6d1b4a31767
Jianzu Guan <git@jzg.fastmail.com>
2024-11-21 11:07:10 +0000

    install oh-my-zsh in unattended mode

--- setupMac.sh
+++ setupMac.sh
@@ -20,7 +20,7 @@ brew install --cask grammarly-desktop
 brew install --cask obsidian
 
 # install on-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # power level 10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
