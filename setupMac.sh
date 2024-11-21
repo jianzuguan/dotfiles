c8218af0048a3a0985406bba97096da32c4e0149
Jianzu Guan <git@jzg.fastmail.com>
2024-11-21 10:41:20 +0000

    add mac setup script

---
+++ setupMac.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# on-my-zsh config
+export ZSH=$HOME/.local/share/oh-my-zsh
+export ZSH_CUSTOM=$ZSH/custom
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install fnm
+brew install git
+brew install stow
+brew install tfenv
+brew install zoxide
+
+brew install --cask alt-tab
+brew install --cask arc
+brew install --cask bitwarden
+brew install --cask grammarly-desktop
+brew install --cask obsidian
+
+# install on-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# power level 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  $ZSH_CUSTOM/themes/powerlevel10k
+
+# zsh plugin
+git clone https://github.com/paulirish/git-open.git \
+  $ZSH_CUSTOM/plugins/git-open
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+  $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+git clone git@github.com:jianzuguan/dotfiles.git \
+  $HOME/
+
+cd $HOME/dotfiles
+git checkout modules
+
+stow git
+stow vim
+stow zsh
