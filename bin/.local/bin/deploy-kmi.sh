2403862f509d99412333fcf76cfd329a144c85fe
Jianzu Guan <git@jzg.fastmail.com>
2025-06-26 09:02:29 +0100

    remove deploy-kmi.sh and update-yaml-and-pr.sh scripts

--- bin/.local/bin/deploy-kmi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./update-yaml-and-pr.sh \
-  ~/projects/kmi-services \
-  services/exception-management/oea/service.oea-test.yaml \
-  exception-management.application.image.tag \
-  $1 \
-  "deploy-exception-management-tag-to-$1" \
-  "Deploy exception-management with tag $1"
